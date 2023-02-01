28171908e52c3d491ff02a4e8b70780533115eb4
Ivan Benegas <ivanbenegas1996@gmail.com>
2023-02-01 13:06:41 -0300

    Agregar archivos de proyecto.

---
+++ OperaWebSite/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace OperaWebSite.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.Nombre = "Bienvenido al sistema de Operas";
+            ViewBag.Fecha = DateTime.Now.ToString();
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ OperaWebSite/Models/Opera.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using OperaWebSite.Validations;
+
+namespace OperaWebSite.Models
+{
+    public class Opera
+    {
+        public int OperaId { get; set; }
+
+        [Required(ErrorMessage = "Campo obligatorio")]
+        [Column(TypeName = "varchar(50)")]
+        [StringLength(50)]
+        public string Title { get; set; }
+
+        [Required(ErrorMessage = "Campo obligatorio")]
+        [Column(TypeName = "varchar(50)")]
+        [StringLength(50)]
+        public string Composer { get; set; }
+
+        [CheckValidYear]
+        public int Year { get; set; }
+
+
+
+    }
+}
